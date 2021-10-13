332c27e006754a40999e523ade8e7b8df53aa7f5
Ganesh Khatri <kh6gnanesh@gmail.com>
2021-10-12 23:07:39 -0500

    w3d2 assignment update

--- week3day2assignment/problem5-node-server-using-http-module/app.js
+++ week3day2assignment/problem5-node-server-using-http-module/app.js
@@ -4,34 +4,40 @@ const url = require('url');
 
 const app = http.createServer(function (req, res) {
     const queryObject = url.parse(req.url, true).query;
-    console.log(queryObject.page);
 
-    if (!queryObject) {
-
-    }
     if (req.url == '/' | req.url == '/index.html') {
-        // if (req.query.page) {
-        // res.write('req');
-        // }
-        // fs.readFile('./index.html', function (err, data) {
-        //     if (err) {
-        //         res.write("Error loading index file");
-        //     }
-        //     else {
-        //         res.write(data);
-        //     }
-        // });
-        res.end();
+        renderPage('index');
+    }
+    else if (queryObject.page) {
+        renderPage('page' + queryObject.page);
+    }
+    else if (req.url == '/page1.html') {
+        renderPage('page1');
+    }
+    else if (req.url == '/page2.html') {
+        renderPage('page2');
+    }
+    else if (req.url == '/page3.html') {
+        renderPage('page3');
     }
     else {
-        // res.write("Invalid URL");
+        res.write("Invalid URL");
         res.end();
     }
-});
 
-// app.on('connection', function (socket) {
-//     console.log("New Connection...")
-// });
+    function renderPage(page) {
+        fs.readFile('./pages/' + page + '.html', function (err, data) {
+            if (err) {
+                res.write("Error loading page");
+            }
+            else {
+                res.write(data);
+            }
+            res.end();
+        });
+    }
+
+});
 
 const server = app.listen(3000, function () {
     console.log("Server runnig at port ", server.address().port);
---
+++ week3day2assignment/problem5-node-server-using-http-module/readme.txt
@@ -0,0 +1,9 @@
+URLs for testing are:
+http://localhost:3000/
+http://localhost:3000/index.html
+http://localhost:3000/page1.html
+http://localhost:3000/page2.html
+http://localhost:3000/page3.html
+http://localhost:3000/index.html?page=1
+http://localhost:3000/index.html?page=2
+http://localhost:3000/index.html?page=3
